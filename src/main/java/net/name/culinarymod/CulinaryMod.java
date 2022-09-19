3604c956ae3ceeb04dc07ce2668421fcd1116ee2
Kyle Zhao <kylezhao101@gmail.com>
2022-09-19 16:50:32 -0700

    added seed oil item

--- src/main/java/net/name/culinarymod/CulinaryMod.java
+++ src/main/java/net/name/culinarymod/CulinaryMod.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.name.culinarymod.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -19,6 +20,8 @@ public class CulinaryMod
     public CulinaryMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
