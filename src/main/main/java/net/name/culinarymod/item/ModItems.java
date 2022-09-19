a4c4221b240583c02476c50a81150c2e268e0dd0
Kyle Zhao <kylezhao101@gmail.com>
2022-09-19 16:48:53 -0700

    added seed oil item

---
+++ src/main/main/java/net/name/culinarymod/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.name.culinarymod.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.name.culinarymod.CulinaryMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, CulinaryMod.MOD_ID);
+
+    //seed oil item
+    public static final RegistryObject<Item> SEEDOIL = ITEMS.register("seedoil",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
