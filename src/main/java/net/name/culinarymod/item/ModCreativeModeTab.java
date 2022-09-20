f8349724b87daa66ef0c43f4f9c78ee4d7d83621
Qien Nathan Cai <qien.nathan@gmail.com>
2022-09-19 23:08:35 -0400

    Change tab icon to countertop

--- src/main/java/net/name/culinarymod/item/ModCreativeModeTab.java
+++ src/main/java/net/name/culinarymod/item/ModCreativeModeTab.java
@@ -2,12 +2,13 @@ package net.name.culinarymod.item;
 
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.name.culinarymod.block.ModBlocks;
 
 public class ModCreativeModeTab {
     public static final CreativeModeTab CULINARY_TAB = new CreativeModeTab("culinarytab") {
         @Override
         public ItemStack makeIcon() {
-            return null;
+            return new ItemStack(ModBlocks.COUNTERTOP_BLOCK.get());
         }
     };
 }
