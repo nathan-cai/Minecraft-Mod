38731617ab20784cc44458ae51f35d35ff068412
Qien Nathan Cai <qien.nathan@gmail.com>
2022-09-19 23:09:24 -0400

    Create ModBlock registry and Countertop Block

--- src/main/java/net/name/culinarymod/block/ModBlocks.java
+++ src/main/java/net/name/culinarymod/block/ModBlocks.java
@@ -1,4 +1,40 @@
 package net.name.culinarymod.block;
 
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.name.culinarymod.CulinaryMod;
+import net.name.culinarymod.item.ModCreativeModeTab;
+import net.name.culinarymod.item.ModItems;
+
+import java.util.function.Supplier;
+
 public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS=
+            DeferredRegister.create(ForgeRegistries.BLOCKS, CulinaryMod.MOD_ID);
+
+    public static final RegistryObject<Block> COUNTERTOP_BLOCK = registerBlock("countertop", () ->
+            new Block(BlockBehaviour.Properties.of(Material.WOOD)), ModCreativeModeTab.CULINARY_TAB);
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                            CreativeModeTab tab) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
 }
