273bd742b85d86abfb27b1f91b981669dcc1fbc8
Qien Nathan Cai <qien.nathan@gmail.com>
2022-09-19 20:56:01 -0400

    Create culinarytab for CreativeModeTab

--- src/main/java/net/name/culinarymod/item/ModCreativeModeTab.java
+++ src/main/java/net/name/culinarymod/item/ModCreativeModeTab.java
@@ -1,4 +1,13 @@
 package net.name.culinarymod.item;
 
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
 public class ModCreativeModeTab {
+    public static final CreativeModeTab CULINARY_TAB = new CreativeModeTab("culinarytab") {
+        @Override
+        public ItemStack makeIcon() {
+            return null;
+        }
+    };
 }
