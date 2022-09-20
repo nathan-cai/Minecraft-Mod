f5a535bea9ff0aee5d15afbe4aa14c4fca3ebf11
Qien Nathan Cai <qien.nathan@gmail.com>
2022-09-20 01:22:35 -0400

    Change culinary tab icon

--- src/main/java/net/name/culinarymod/item/ModCreativeModeTab.java
+++ src/main/java/net/name/culinarymod/item/ModCreativeModeTab.java
@@ -2,13 +2,16 @@ package net.name.culinarymod.item;
 
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
 import net.name.culinarymod.block.ModBlocks;
 
 public class ModCreativeModeTab {
     public static final CreativeModeTab CULINARY_TAB = new CreativeModeTab("culinarytab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModBlocks.COUNTERTOP_BLOCK.get());
+            return Items.COOKED_BEEF.getDefaultInstance();
+//            return new ItemStack(ModBlocks.COUNTERTOP_BLOCK.get());
         }
     };
 }
