ed5962b8ccb67445d79c93a674a92d7eee16eec7
gavinzsim <gavinzsschool@gmail.com>
2022-09-29 22:26:29 -0700

    Completed minecraft knife

--- src/main/java/net/name/culinarymod/item/ModItems.java
+++ src/main/java/net/name/culinarymod/item/ModItems.java
@@ -1,7 +1,6 @@
 package net.name.culinarymod.item;
 
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,6 +14,11 @@ public class ModItems {
     //seed oil item
     public static final RegistryObject<Item> SEEDOIL = ITEMS.register("seedoil",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    //knife item
+    public static final RegistryObject<Item> KNIFE = ITEMS.register("knife",
+            () -> new SwordItem(Tiers.IRON,10,5f,
+                    new Item.Properties().tab(ModCreativeModeTab.CULINARY_TAB).stacksTo(1)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
