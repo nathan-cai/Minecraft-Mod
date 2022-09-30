f794f914fcad0f1365865e8886dc86b9abaef01b
Qien Nathan Cai <qien.nathan@gmail.com>
2022-09-30 02:46:43 -0400

    register beefslab item

--- src/main/java/net/name/culinarymod/item/ModItems.java
+++ src/main/java/net/name/culinarymod/item/ModItems.java
@@ -1,5 +1,6 @@
 package net.name.culinarymod.item;
 
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -9,14 +10,17 @@ import net.minecraftforge.registries.RegistryObject;
 import net.name.culinarymod.CulinaryMod;
 
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS =
-            DeferredRegister.create(ForgeRegistries.ITEMS, CulinaryMod.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+            CulinaryMod.MOD_ID);
 
-    //seed oil item
+    // seed oil item
     public static final RegistryObject<Item> SEEDOIL = ITEMS.register("seedoil",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
-    public static void register(IEventBus eventBus){
+    public static final RegistryObject<Item> BEEFSLAB = ITEMS.register("beefslab",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CULINARY_TAB).food(ModFoods.BEEFSLAB)));
+
+    public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
 }
